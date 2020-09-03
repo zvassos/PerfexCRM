24251d036108c5a4191cebc71aa8af17e4644ec0
Zissis Vassos <zissis21@gmail.com>
2020-09-03 13:17:33 +0300

    Updated greek_lang.php to Versioν 2.7.0

--- greek/greek_lang.php
+++ greek/greek_lang.php
@@ -1782,7 +1782,6 @@ $lang['ticket_pipe_status']   = 'Κατάσταση';
 # Home
 $lang['home_latest_activity']   = 'Κινήσεις CRM';
 $lang['home_my_tasks']          = 'Task μου';
-$lang['home_latest_activity']   = 'Κινήσεις CRM';
 $lang['home_my_todo_items']     = 'To do\'s';
 $lang['home_widget_view_all']   = 'Εμφάνιση όλων';
 $lang['home_stats_full_report'] = 'Πλήρης αναφορά';
@@ -2603,15 +2602,10 @@ $lang['task_single_log_user'] = $lang['project_timesheet_user'];
 $lang['milestone_description']                      = 'Περιγραφή';
 $lang['description_visible_to_customer']            = 'Δείξτε περιγραφή στον πελάτη';
 $lang['upcoming_tasks']                             = 'Επόμενα Task';
-$lang['paymentmethod_two_checkout_account_number']  = 'Αριθμός Λογαριασμού (ID Πωλητή)';
-$lang['paymentmethod_two_checkout_private_key']     = 'Ιδιωτικό Κλειδί';
-$lang['paymentmethod_two_checkout_publishable_key'] = 'Δημοσιεύσιμο Κλειδί';
 $lang['payment_credit_card_number']                 = 'Αριθμός Κάρτας';
 $lang['payment_credit_card_expiration_date']        = 'Ημερομηνία Λήξης';
 $lang['payment_billing_email']                      = 'Email';
 $lang['submit_payment']                             = 'Υποβολή πληρωμής';
-$lang['2checkout_notice_payment']                   = 'Τα στοιχεία της πιστωτικής κάρτας επεξεργάστηκαν μέσω ασφαλούς ένδειξης που παρέχεται από 2Checkout';
-$lang['2checkout_usage_notice']                     = 'Το SSL απαιτείται εάν χρησιμοποιείτε το API πληρωμής 2Checkout. Απαιτείται να καλέσετε με ασφαλή τρόπο το tokenization και τις εξουσιοδοτήσεις. Το σύστημα θα λειτουργήσει χωρίς SSL, ωστόσο, αυτό θα σας απομακρύνει από τη συμμόρφωση, γεγονός που διακινδυνεύει την απενεργοποίηση των δικαιωμάτων API σας';
 $lang['custom_field_disallow_customer_to_edit']     = 'Απενεργοποιήστε τον πελάτη για να επεξεργαστείτε αυτό το πεδίο';
 $lang['project_due_notice']                         = 'Αυτό το project λήγει σε %s μέρες';
 $lang['not_lead_added_attachment']                  = 'πρόσθήκη νέου επισυναπτόμενου σε lead %s';
@@ -2662,7 +2656,7 @@ $lang['save_as_draft']                                      = 'ΑΠΟΘΗΚΕΥ�
 $lang['convert_and_save_as_draft']                          = 'Μετατροπή και αποθήκευση ως πρότυπο';
 $lang['convert']                                            = 'ΜΕΤΑΤΡΟΠΗ';
 $lang['exclude_invoices_draft_from_client_area']            = 'Απόκρυψη από τον πελάτη των τιμολογίων που έχουν αποθηκευτεί ως "Πρόχειρο".';
-$lang['invoice_draft_status_info']                          = 'Το τιμολόγιο έχει κατάσταση Πρόχειρο. Η κατάσταση θα αλλάξει αυτόματα όταν στείλετε το τιμολόγιο στον πελάτη ή το σημειώσετε  ως απεσταλμένο.';
+$lang['invoice_draft_status_info']                          = 'Το τιμολόγιο είναι πρόχειρο. Η κατάσταση θα αλλάξει αν σταλεί ή αν γίνει απεσταλμένο.';
 $lang['task_info']                                          = 'Πληροφορίες task';
 $lang['recurring_tasks']                                    = 'Επαναλαμβανόμενο';
 // don't translate these, already translated
@@ -2761,7 +2755,6 @@ $lang['no_leads_found']                              = 'Δεν βρέθηκαν 
 $lang['created_today']                               = 'Δημιουργήθηκαν σήμερα';
 $lang['total_tasks_deleted']                         = 'Συνολικά διαγραμμένα task: %s';
 $lang['total_tickets_delete']                        = 'Συνολικά διαγραμμένα tickets: %s';
-$lang['total_tickets_delete']                        = 'Συνολικά tickets διαγράφονται: %s';
 $lang['format_letter_portrait']                      = 'Κάθετη εμφάνιση';
 $lang['format_letter_landscape']                     = 'Οριζόντια εμφάνιση';
 $lang['period_datepicker']                           = 'Περίοδος';
@@ -3456,7 +3449,6 @@ $lang['view_consent']                                           = 'Προβολ�
 $lang['transfer_consent']                                       = 'Μεταφορά συγκατάθεσης';
 $lang['view_public_form']                                       = 'Προβολή δημόσιας φόρμας';
 $lang['update_consent']                                         = 'Ενημέρωση συγκατάθεσης';
-$lang['update_consent']                                         = 'Ενημέρωση συγκατάθεσης';
 $lang['consent_last_updated']                                   = 'Τελευταία ενημέρωση: %s';
 $lang['showing_search_result']                                  = 'Αποτελέσματα για: %s';
 $lang['per_page']                                               = 'Για κάθε σελίδα';
@@ -3520,7 +3512,7 @@ $lang['next_invoice_date_list'] = 'Ημερομηνία επόμενου τιμ�
 $lang['enter_new_card']         = 'Προσθέστε νέα κάρτα';
 
 # Version 2.2.0
-$lang['save_and_record_payment']                                     = 'Save & Record Payment';
+$lang['save_and_record_payment']                                     = 'Αποθήκευση & Καταγραφή Πληρωμής';
 $lang['choose_from_google_drive']                                    = 'Επιλέξτε από το Google Drive';
 $lang['open_in_google']                                              = 'Open In Google';
 $lang['google_picker']                                               = 'Google Picker';
@@ -3567,38 +3559,109 @@ $lang['sales_item']                  = 'Προϊόν';
 
 $lang['modules']                                 = 'Modules';
 $lang['module']                                  = 'Module';
-$lang['module_description']                      = 'Description';
-$lang['module_activate']                         = 'Activate';
-$lang['module_deactivate']                       = 'Deactivate';
-$lang['module_uninstall']                        = 'Uninstall';
-$lang['module_upgrade_database']                 = 'Upgrade Database';
-$lang['module_settings']                         = 'Settings';
-$lang['module_version']                          = 'Version %s';
-$lang['module_by']                               = 'By %s';
-$lang['staff_which_are_using_role']              = 'Staff members which are using this role';
-$lang['copy']                                    = 'Copy';
+$lang['module_description']                      = 'Περιγραφή';
+$lang['module_activate']                         = 'Ενεργοποίηση';
+$lang['module_deactivate']                       = 'Απενεργοποίηση';
+$lang['module_uninstall']                        = 'Απεγκατάσταση';
+$lang['module_upgrade_database']                 = 'Αναβάθμιση βάσης δεδομένων';
+$lang['module_settings']                         = 'Ρυθμίσεις';
+$lang['module_version']                          = 'Έκδοση %s';
+$lang['module_by']                               = 'Βάση %s';
+$lang['staff_which_are_using_role']              = 'Τα μέλη του προσωπικού που χρησιμοποιούν αυτόν τον ρόλο';
+$lang['copy']                                    = 'Αντιγραφή';
 $lang['read_more']                               = 'ΔΕΙΤΕ ΠΕΡΙΣΣΟΤΕΡΑ';
 $lang['show_less']                               = 'ΔΕΙΤΕ ΛΙΓΟΤΕΡΑ';
 $lang['project_progress_text']                   = 'ΠΡΟΟΔΟΣ PROJECT';
-$lang['timer_not_stopped_yet']                   = 'This timer is not yet stopped';
-$lang['refunds']                                 = 'Refunds';
-$lang['refund']                                  = 'Refund';
-$lang['refund_amount']                           = 'Refunded Amount';
-$lang['not_refunds_found']                       = 'No refunds found';
-$lang['refunds_applied_cant_delete_credit_note'] = 'This credit note has refunds applied, you need first to delete the refunds in order to delete the credit note.';
+$lang['timer_not_stopped_yet']                   = 'Αυτό το χρονόμετρο δεν έχει σταματήσει ακόμα';
+$lang['refunds']                                 = 'Επιστροφές χρημάτων';
+$lang['refund']                                  = 'Επιστροφή χρημάτων';
+$lang['refund_amount']                           = 'Επιστρεφόμενο ποσό';
+$lang['not_refunds_found']                       = 'Δεν βρέθηκαν επιστροφές χρημάτων';
+$lang['refunds_applied_cant_delete_credit_note'] = 'Αυτό το πιστωτικό σημείωμα έχει επιστροφές που εφαρμόζονται, πρέπει πρώτα να διαγράψετε τις επιστροφές χρημάτων για να διαγράψετε το πιστωτικό σημείωμα.';
 
 # Version 2.3.2
-$lang['create_recurring_from_child_error_message'] = 'You cannot set this %s as recurring because this %s is child from another recurring %s.';
-$lang['statement_credit_note_refund']              = 'Credit Note Refund - %s';
-$lang['no_validation']                             = 'No Validation';
-$lang['lead_unique_validation_on']                 = 'Perform validation for duplicate lead on the following fields:';
-$lang['phonenumber_exists']                        = 'Phone number already exists';
-$lang['company_exists']                            = 'Company already exists';
-$lang['website_exists']                            = 'Website already exists';
-$lang['send_payment_receipt_to_client']            = 'Send Payment Receipt To Client';
-$lang['payment_sent_successfully']                 = 'Payment receipt sent successfully.';
-$lang['payment_sent_failed']                       = 'Failed to send payment receipt.';
+$lang['create_recurring_from_child_error_message'] = 'Δεν μπορείτε να ορίσετε αυτό το %s ως επαναλαμβανόμενο επειδή ανήκει σε άλλο επαναλαμβανόμενο %s.';
+$lang['statement_credit_note_refund']              = 'Επιστροφή Πιστωτικής Σημείωσης - %s';
+$lang['no_validation']                             = 'Χωρίς εξακρύβωση';
+$lang['lead_unique_validation_on']                 = 'Εκτελέστε επικύρωση για διπλό οδηγό στα παρακάτω πεδία:';
+$lang['phonenumber_exists']                        = 'Το τηλέφωνο υπάρχει ήδη';
+$lang['company_exists']                            = 'Η εταιρεία υπάρχει ήδη';
+$lang['website_exists']                            = 'Η ιστοσελίδα υπάρχει ήδη';
+$lang['send_payment_receipt_to_client']            = 'Αποστολή απόδειξης πληρωμής';
+$lang['payment_sent_successfully']                 = 'Επιτυχής αποστολή απόδειξης.';
+$lang['payment_sent_failed']                       = 'Ανεπιτυχής αποστολή απόδειξης.';
 
 # Version 2.3.5
-$lang['tags_update_replace_warning'] = 'Some tags are not updated because the name of the tag already exist';
-$lang['attach_statement']            = 'Attach Customer Statement';
+$lang['tags_update_replace_warning'] = 'Ορισμένες ετικέτες δεν ενημερώνονται επειδή το όνομα της ετικέτας υπάρχει ήδη';
+$lang['attach_statement']            = 'Επισύναψη δήλωσης πελάτη';
+
+# Version 2.4.0
+$lang['delete_credit_card']               = 'Διαγραφή κάρτας';
+$lang['delete_credit_card_info']          = 'Δεν μπορείτε να διαγράψετε αυτήν την κάρτα καθώς έχετε ενεργές συνδρομές.';
+$lang['credit_card_successfully_deleted'] = 'Η κάρτα διαγράφηκε με επιτυχία.';
+$lang['subscription_incomplete']          = 'Ατελής';
+$lang['subscription_incomplete_expired']  = 'Λήξη ατελούς συνδρομής';
+$lang['credit_card_short']                = 'Κάρτα';
+$lang['webhook_created']                  = 'Το Webhook δημιουργήθηκε με επιτυχία.';
+$lang['subscriptions_terms_info']         = 'Εισαγάγετε τους όρους και τις προϋποθέσεις του πελάτη για να εμφανιστούν στον πελάτη προτού κάνει συνδρομή.';
+$lang['subscription_complete_payment']    = 'Ολοκλήρωση πληρωμής';
+$lang['subscription_is_subscription_is_expired'] = 'Αυτή η συνδρομή έχει λήξει.';
+$lang['subscription_plan_currency_does_not_match'] = 'Το επιλεγμένο νόμισμα του προγράμματος δεν ταιριάζει με το επιλεγμένο νόμισμα παρακάτω.';
+$lang['subscription_first_billing_date_info'] = 'Αφήστε κενό για να χρησιμοποιήσετε την ημερομηνία όταν ο πελάτης είναι εγγεγραμμένος στη συνδρομή. Το πεδίο αυτό πρέπει να είναι μελλοντική ημερομηνία, εάν επιλέξετε ημερομηνία και η ημερομηνία λήξει, αλλά ο πελάτης δεν έχει ακόμη εγγραφεί, θα χρησιμοποιηθεί η ημερομηνία που θα εγγραφεί ο πελάτης.';
+$lang['stripe_subscription_select_plan'] = 'Επιλογή πακέτου Stripe';
+
+# Version 2.4.1
+$lang['contract_content_permission_edit_warning'] = 'Τα τρέχοντα σας δικαιώματα δεν σας επιτρέπουν να επεξεργαστείτε το περιεχόμενο του συμβολαίου. Συμβουλευτείτε τον administrtor για να έχετε άδεια να επεξεργαστείτε συμβόλαια.';
+$lang['mark_as_signed']                 = 'Μαρκάρετε ως υπογεγραμμένο';
+$lang['unmark_as_signed']               = 'Κατάργηση σήμανσης ως υπογεγραμμένo';
+$lang['marked_as_signed']               = 'Υπογεγραμμένο';
+$lang['contract_marked_as_signed_info'] = 'Αυτο το συμβόλαιο έχει επισημανθεί με μη αυτόματο τρόπο ως υπογεγραμμένο.';
+$lang['save_and_send_later']            = 'Αποθηκεύστε και στείλτε αργότερα';
+$lang['schedule']                       = 'Προγραμματισμός';
+$lang['schedule_email_for']             = 'Προγραμματισμός email για %s';
+$lang['schedule_date']                  = 'Πότε θέλετε να στείλετε το email;';
+$lang['email_scheduled_successfully']   = 'Το email έχει προγραμματιστεί με επιτυχία';
+$lang['invoice_will_be_sent_at']        = 'Το τιμολόγιο θα αποσταλεί στις %s';
+
+# Version 2.5.0
+$lang['recaptcha_ignore_ips']               = 'Διευθύνσεις IP που αγνοήθηκαν';
+$lang['recaptcha_ignore_ips_info']          = 'Εισαγάγετε διευθύνσεις IP διαχωρισμένες με κόμμα που θέλετε να παραλείψει την επικύρωση το reCaptcha.';
+$lang['show_task_reminders_on_calendar']    = 'Υπενθυμίσεις task';
+$lang['contracts_about_to_expire']          = 'Συμβόλαια που λήγουν σύντομα';
+$lang['no_contracts_about_to_expire']       = 'Δεν υπάρχουν συμβόλαια που πρόκειται να λήξουν τις επόμενες %s ημέρες.';
+$lang['lead_value']                         = 'Αξία Lead';
+$lang['lead_value_tooltip']                 = 'Το βασικό νόμισμα θα χρησιμοποιηθεί.';
+$lang['leads_dt_lead_value']                = 'Αξία Lead';
+$lang['leads_canban_lead_value']            = 'Αξία Lead: %s';
+$lang['lead_add_edit_lead_value']           = 'Αξία Lead';
+
+# Version 2.6.0
+$lang['gantt_view_day'] = 'Προβολή ημερών';
+$lang['gantt_view_week'] = 'Προβολή εβδομάδων';
+$lang['gantt_view_month'] = 'Προβολή μηνών';
+$lang['gantt_view_year'] = 'Προβολή ετών';
+
+# Version 2.7.0
+$lang['hour_of_day_perform_tasks_reminder_notification_help'] = 'Μορφή 24 ωρών π.χ. 9 για 9πμ ή 15 για 3μμ. Χρησιμοποιείται για επαναλαμβανόμενες εργασίες, υπενθυμίσεις εργασιών κ.λπ.';
+$lang['clients_nav_contacts']                                 = 'Επαφές';
+$lang['clients_my_contacts']                                  = 'Επαφές';
+$lang['clients_my_contact']                                   = 'Επαφή';
+$lang['new_contact']                                          = 'Νέα επαφή';
+$lang['customer_contact']                                     = 'Οι επαφές μου';
+$lang['clients_contact_added']                                = 'Η επαφή προστέθηκε με επιτυχία';
+$lang['clients_contact_updated']                              = 'Η επαφή ενημερώθηκε με επιτυχία';
+$lang['allow_primary_contact_to_manage_other_contacts']       = 'Επιτρέψτε στην κύρια επαφή να διαχειρίζεται άλλες επαφές πελατών';
+$lang['contact_form_validation_is_unique']                    = 'Η επαφή με {field} υπάρχει ήδη στο σύστημά μας';
+$lang['invoice_number_not_applied_on_draft']                  = 'Εάν το τιμολόγιο αποθηκευτεί ως πρόχειρο, ο αριθμός δεν θα εφαρμοστεί, αντ \'αυτού, ο επόμενος αριθμός τιμολογίου θα δοθεί όταν το τιμολόγιο αποσταλεί στον πελάτη ή επισημανθεί ως απεσταλμένο.'
+$lang['two_factor_authentication_disabed']                    = 'Απενεργοποιημένο';
+$lang['enable_google_two_factor_authentication']              = 'Ενεργοποιήστε Google Authentication';
+$lang['set_google_two_factor_authentication_failed']          = 'Η αποθήκευση έλεγχου ταυτότητας απέτυχε, παρακαλούμε προσπαθήστε ξανά';
+$lang['enter_two_factor_auth_code_from_mobile']               = 'Προσθέστε τον κωδικό έλεγχου ταυτότητας από την εφαρμογή Google Authenticator.';
+$lang['staff_two_factor_authentication']                      = 'Έλεγχος ταυτότητας δύο παραγόντων';
+$lang['google_authentication_code']                           = 'Προσθέστε τον κωδικό από την εφαρμογή Google Authenticator';
+$lang['set_two_factor_authentication_successful']             = 'Ενημερώθηκε με επιτυχία οι ρυθμίσεις ελέγχου ταυτότητας δύο παραγόντων';
+$lang['set_two_factor_authentication_failed']                 = 'Δεν ήταν δυνατή η ενημέρωση των ρυθμίσεων ελέγχου ταυτότητας δύο παραγόντων';
+$lang['google_2fa_code_valid']                                = 'Επιβεβαιώθηκε με επιτυχία ο έλεγχος ταυτότητας';
+$lang['google_2fa_code_invalid']                              = 'Λάθος κωδικός έλεγχου ταυτότητας, προσπαθήστε ξανά.';
+$lang['google_2fa_scan_qr_guide']                             = 'Σαρώστε το QR παρακάτω με την εφαρμογή Google Authenticator στην κινητή συσκευή σας, μετά συμπληρώστε το παρακάτω πεδίο με τον κωδικό που δημιουργείται στην εφαρμογή';
+$lang['google_2fa_manul_input_secret']                        = 'Μυστικό κλειδί για χειροκίνητη εισαγωγή';
